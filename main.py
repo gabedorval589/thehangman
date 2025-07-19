4f4e4a404303b4a272eecc8ae2aeb27d052a56a4
Gabe Dorval <gabed589@gmail.com>
2025-07-19 11:16:31 -0400

    Deleted function's test call.

--- main.py
+++ main.py
@@ -5,6 +5,3 @@ def get_random_word(word_list):
   """ This function returns a random string from the passed list of strings."""
   word_index = random.randint(0, len(word_list) - 1)
   return word_list[word_index]
-
-# print(get_random_word(hm_module.words))
-
